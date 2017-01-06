0b4a007680b87b0d2801a3a7517397ff50c2a2e5
mlunoe <michael.lunoe@gmail.com>
2017-01-05 19:27:16 -0800

    Cleanup index.html and services.js

--- app/js/services.js
+++ app/js/services.js
@@ -2,27 +2,24 @@
    "use strict";
 
   /* Services */
-
   angular.module('m.lunoe.services', [])
 
     .factory('GitHubService', ['$http', function($http) {
       var Response = {
         data: {},
         get: function() {
-          var config = {
-            params : {
-              callback: "JSON_CALLBACK"
-            }
-        };
-        return $http.jsonp('https://api.github.com/users/mlunoe/events', config)
-          .success(function(result) {
-            Response.data = result.data;
-          })
-          .error(function(data, status, headers) {
-            Response.data = status;
-          });
+          var config = {params: {callback: "JSON_CALLBACK"}};
+
+          return $http.jsonp('https://api.github.com/users/mlunoe/events', config)
+            .success(function(result) {
+              Response.data = result.data;
+            })
+            .error(function(data, status, headers) {
+              Response.data = status;
+            });
         }
       };
+
       return Response;
     }])
 
@@ -30,11 +27,8 @@
       var Response = {
         data: {},
         get: function() {
-          var config = {
-            params : {
-              callback: "JSON_CALLBACK"
-            }
-        };
+        var config = {params: {callback: "JSON_CALLBACK"}};
+
         return $http.jsonp('https://api.stackexchange.com/2.2/users?order=desc&sort=name&inname=mlunoe&site=stackoverflow', config)
           .success(function(result) {
             Response.data = result.data;
@@ -44,12 +38,14 @@
           });
         }
       };
+
       return Response;
     }])
 
     .factory('socket', function ($rootScope) {
-      var socket = io.connect('http://m.lunoe.dk:8000');
       // var socket = io.connect('http://localhost:8000');
+      var socket = io.connect('http://m.lunoe.dk:8000');
+
       return {
         on: function (eventName, callback) {
           socket.on(eventName, function () {
